e64bc4b8a55ce5367dbaaa341d80abefa4b176a9
alineerthal <aerp.14@gmail.com>
2022-05-23 15:30:28 -0300

    adicionando o arquivo funcoesImportantes.js

---
+++ exercicios-js/objeto/funcoesImportantes.js
@@ -0,0 +1,39 @@
+const pessoa = {
+    nome: 'Rebeca',
+    idade: 2,
+    peso: 13
+}
+
+console.log(Object.keys(pessoa))
+console.log(Object.values(pessoa))
+console.log(Object.entries(pessoa)) // saída de arrays com os sub arrays contendo chave/valor
+
+Object.entries(pessoa).forEach(e => {
+    console.log(`${e[0]}: ${e[1]}`)
+})
+
+//ou 
+// usando o destructuring
+Object.entries(pessoa).forEach(([chave, valor]) => {
+    console.log(`${chave}: ${valor}`)
+})
+
+Object.defineProperty(pessoa, 'dataNascimento', {
+    enumerable: true, // vai aparecer na lista de chave/valor
+    writable: false, //n aceita ser sobrescrita
+    value: '01/01/2019'
+})
+
+pessoa.dataNascimento = '01/01/2017'
+console.log(pessoa.dataNascimento)
+console.log(Object.keys(pessoa))
+
+// Object.assign (ECMAScript 2015)
+const dest = { a: 1 }
+const o1 = { b: 2 }
+const o2 = { c: 3, a: 4 }
+const obj = Object.assign(dest, o1, o2)
+
+Object.freeze(obj)
+obj.c = 1234
+console.log(obj)
